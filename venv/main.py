39e63d6d2fce091c8623ec8f8d1d2a6f35405847
Administrator <412108791@qq.com>
2021-04-14 14:33:21 +0800

    pytest参数化

--- venv/main.py
+++ venv/main.py
@@ -2,5 +2,7 @@
 print("This is a test")
 print("This is a test")
 print("This is a test add a lineqq")
-print("This is a test add a lineqq")print("This is a test add a lineqq")
+print("This is a test add a lineqq")
+print("This is a test add a lineqq")
+print("This is a test add a lineqq   for jenkins")
 
